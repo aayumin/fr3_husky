47d287fb9736f81a1cc592cdbbe8340aa158edf3
aayumin <ckrgksakdmac@gmail.com>
2026-05-24 23:15:48 +0900

    [Add new action server]  Task space delta move.

--- fr3_husky_task_manager/setup.py
+++ fr3_husky_task_manager/setup.py
@@ -28,6 +28,7 @@ setup(
             'husky_pedal = fr3_husky_task_manager.husky_pedal:main',
             'move_to_joint = fr3_husky_task_manager.move_to_joint:main',
             'task_space_move = fr3_husky_task_manager.task_space_move:main',
+            'task_space_delta_move = fr3_husky_task_manager.task_space_delta_move:main',
             'gripper_move = fr3_husky_task_manager.gripper_move:main',
             'log = fr3_husky_task_manager.live_log:main',
             'image = fr3_husky_task_manager.image_streaming:main',
